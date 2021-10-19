0c637baeecfcecc02dc88816248ce4df91449e2d
abubakar.daud@mail.mcgill.ca <abubakar.daud@mail.mcgill.ca>
2021-10-19 18:57:51 -0400

    first commit

---
+++ cogs/verifysystem.py
@@ -0,0 +1,86 @@
+import discord
+from discord.ext import commands
+bot = commands.Bot(command_prefix='~')
+
+class VerificationCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    @commands.guild_only()
+    async def joined(self, ctx, *, member: discord.Member):
+        """Says when a member joined."""
+        await ctx.send(f'{member.display_name} joined on {member.joined_at}')
+
+    @commands.command(name='creator')
+    async def creator(self, ctx):
+        """Is the bot cool?"""
+        await ctx.send('Made with ❤️ by Abu and Diego~!! ')
+
+    @commands.command(name='opensource')
+    async def opensource(self, ctx):
+        """text for open source link"""
+        await ctx.send('Project is completely **open source**, \n\n check out the ✨github: https://github.com/abubakardaud/McGillDiscordProject')
+
+    @commands.command(name='cutest')
+    async def cutest(self, ctx):
+        """Is the bot cool?"""
+        await ctx.send('Christina is the cutest~!!')
+
+
+    @commands.command(name='verify')
+    @commands.guild_only()
+    async def verify(self, ctx):
+        """Is the bot cool?"""
+        await ctx.send('This bot is cool :)')
+
+
+    @commands.command(name='top_role', aliases=['toprole'])
+    @commands.guild_only()
+    async def show_toprole(self, ctx, *, member: discord.Member = None):
+        """Simple command which shows the members Top Role."""
+
+        if member is None:
+            member = ctx.author
+
+        await ctx.send(f'The top role for {member.display_name} is {member.top_role.name}')
+
+    @commands.command(name='perms', aliases=['perms_for', 'permissions'])
+    @commands.guild_only()
+    async def check_permissions(self, ctx, *, member: discord.Member = None):
+        """A simple command which checks a members Guild Permissions.
+        If member is not provided, the author will be checked."""
+
+        if not member:
+            member = ctx.author
+
+        # Here we check if the value of each permission is True.
+        perms = '\n'.join(perm for perm, value in member.guild_permissions if value)
+
+        # And to make it look nice, we wrap it in an Embed.
+        embed = discord.Embed(title='Permissions for:', description=ctx.guild.name, colour=member.colour)
+        embed.set_author(icon_url=member.avatar_url, name=str(member))
+
+        # \uFEFF is a Zero-Width Space, which basically allows us to have an empty field name.
+        embed.add_field(name='\uFEFF', value=perms)
+
+        await ctx.send(content=None, embed=embed)
+        # Thanks to Gio for the Command.
+
+
+# The setup fucntion below is neccesarry. Remember we give bot.add_cog() the name of the class in this case MembersCog.
+# When we load the cog, we use the name of the file.
+def setup(bot):
+    bot.add_cog(VerificationCog(bot))
+
+
+
+
+
+
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,7 +1,25 @@
-import os
-from verifysystem import VerificationSystem
+#from gitingore import SECERT
+from discord.ext import commands
+import discord
+bot = commands.Bot(command_prefix='~')
+
+initial_extensions = ['cogs.verifysystem',
+                      ]
+
+
+@bot.event
+async def on_ready():
+    """http://discordpy.readthedocs.io/en/rewrite/api.html#discord.on_ready"""
+
+    print(f'\n\nLogged in as: {bot.user.name} - {bot.user.id}\nVersion: {discord.__version__}\n')
+    # Changes our bots Playing Status. type=1(streaming) for a standard game you could remove type and url.
+    print(f'Successfully logged in and booted...!')
+
 
 if __name__ == "__main__":
     key = os.environ["SecertClientKey"]
+    # key = SECERT
+    for extension in initial_extensions:
+        bot.load_extension(extension)
 
-    VerificationSystem.run(key)
+    bot.run(key, bot=True, reconnect=True)
